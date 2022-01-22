3ac8c6315949b31c3af6374510f49f0eff815302
Connor <connorking.milloy@gmail.com>
2022-01-22 03:30:10 +0000

    imported createHandImage logic and invoked it every 8 hours

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { createHandImage } = require('./createHand');
 
 const { Client, Intents } = require('discord.js');
 const client = new Client( {intents: [Intents.FLAGS.GUILDS ]});
@@ -7,6 +8,15 @@ const prefix = '.';
 client.login(process.env.DISCORD_TOKEN);
 
 client.on('ready', async () => {
+    console.log('Bot online!');
+    const delay = 1000 * 60 * 60 * 8;
     const channel = await client.channels.fetch("812154155615125517");
 
+    setInterval(() => {
+        createHandImage()
+            .then(() => {
+                channel.send({files: ['../hand.png']})
+            })
+            .catch(e => console.log(e))
+    }, delay)
 })
